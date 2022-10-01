6971ed16463ab86c14caf0218083125eb1517b76
mod-on <76691082+mod-on@users.noreply.github.com>
2022-10-02 00:36:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import time
 print("Взлом пароля от аккаунта")
-n = int(input("Введите имя пользователя"))
+n = input("Введите имя пользователя")
 for i in range(0,1001,1):
     print("Перебор паролей",str(i/10)+"%")
-    time.sleep(0.001)
+    time.sleep(0.0001)
 	
 print("Ошибка, нехватает данных")
 if input("2+2=") == "4":
     for i in range(0,100):
         print("Перебор паролей",str(i)+"%")
-        time.sleep(0.1)
-        print("Пароль: 1234")
-        time.sleep(1000)
+        time.sleep(0.01)
+    print("Пароль: 1234")
+    time.sleep(1000)
 else:
     print("Неверные данные")
     time.sleep(1000)
